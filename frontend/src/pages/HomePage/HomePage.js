c17b8e21952c65892965f43b2d2a4f8c3f21d1e9
Martin Isaksen <izaxen@telia.com>
2022-05-02 20:32:57 +0200

    Initial commit

---
+++ frontend/src/pages/HomePage/HomePage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const HomePage = () => {
+  return (
+    <div>
+      Att göra:<br></br>
+      Skapa huvudadmin<br></br>
+    </div>
+  )
+}
+
+export default HomePage
